9484d595cf368d472d2f9f059e4bd7d8d3b0c16c
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-07-24 14:58:46 +0530

    Added constructors to User entity for JPA and application use

--- backend/src/main/java/com/sprintsphere/backend/entity/User.java
+++ backend/src/main/java/com/sprintsphere/backend/entity/User.java
@@ -19,4 +19,13 @@ public class User {
     private String email;
 
     private String password;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
